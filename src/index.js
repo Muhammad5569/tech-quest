cb1f601b44676b1d3d4dcfe9908dc5aa2bdf3632
Muhammad5569 <777muhammad5569@gmail.com>
2025-03-24 04:06:04 +0500

    cors

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ app.use(express.json())
 app.use(userRouter)
 app.use(questRouter)
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
 
 
