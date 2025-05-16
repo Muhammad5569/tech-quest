90f7a99836bf8c23b628ff614e41043d9d5facba
Muhammad5569 <777muhammad5569@gmail.com>
2025-05-16 17:54:43 +0500

    cookie

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: '*',
+  origin: ['https://tech-quest.uz', 'http://localhost'],
   credentials: true  
 }));
 
